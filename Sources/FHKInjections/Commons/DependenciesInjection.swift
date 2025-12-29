fbd250409675802b4d17a554cde01414d8458ffe
Fredy Leon <fredy.leon@ua.la.com>
2025-12-29 16:22:25 +0100

    move protocol

---
+++ Sources/FHKInjections/Commons/DependenciesInjection.swift
@@ -0,0 +1,21 @@
+//
+//  DependenciesInjection.swift
+//  FHKInjections
+//
+//  Created by Fredy Leon on 29/12/25.
+//
+
+import Foundation
+
+public protocol LanguageManagerProtocol: ObservableObject, Sendable {
+    var selectedLanguage: String { get }
+    func readLanguage() async
+    func saveLanguage(_ language: String) async
+}
+
+extension DependenciesInjection {
+    var languageManager: any LanguageManagerProtocol {
+        get { self[(any LanguageManagerProtocol).self] }
+        set { self[(any LanguageManagerProtocol).self] = newValue }
+    }
+}
