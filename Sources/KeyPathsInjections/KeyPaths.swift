7bd2df94bb63d3721b86940a3507b459b393b30c
Fredy Leon <fredy.leon@ua.la.com>
2026-03-07 09:57:50 +0100

    test move keyPaths

---
+++ Sources/KeyPathsInjections/KeyPaths.swift
@@ -0,0 +1,13 @@
+//
+//  KeyPaths.swift
+//  FHKInjections
+//
+//  Created by Fredy Leon on 7/3/26.
+//
+
+import FHKDomain
+
+var storageManager: any FHKStorageManagerProtocol {
+    get { get((any FHKStorageManagerProtocol).self) }
+    set { set(newValue, for: (any FHKStorageManagerProtocol).self) }
+}
